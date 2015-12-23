a5749cdce5a15a8b66b67de4fa3a6532f49db79b
yoont4 <taylor.y1100@gmail.com>
2015-12-22 23:40:36 -0800

    updated playercontroller and added audio

--- character/GroundChecker.cs
+++ character/GroundChecker.cs
@@ -3,18 +3,35 @@ using System.Collections;
 
 public class GroundChecker : MonoBehaviour {
 
+    public float landingThreshold;
+
 	private PlayerController player;
+    private Rigidbody2D playerBody;
 
 	// Use this for initialization
 	void Start () {
 		player = this.GetComponentInParent<PlayerController> ();
+        playerBody = this.GetComponentInParent<Rigidbody2D>();
 	}
 
 
 	void OnTriggerEnter2D(Collider2D col) {
-        player.animator.SetTrigger("startLand");
-		player.onGround = true;
-		player.onFirstJump = false;
+
+        // check if the colliding layer matches any of the ground layers
+        int colBit = 1<<col.gameObject.layer;
+        int overlap = player.groundLayer.value & colBit;
+        if (overlap > 0) {
+            player.onGround = true;
+		    player.onFirstJump = false;
+            
+            // only trigger landing animation if player is falling fast enough
+            // calculate landing threshold
+            if (player.fallSpeed <= -landingThreshold) {
+                player.animator.SetTrigger("startLand");
+            } 
+        }
+
+
 	}
 
 	// double check on ground (not needed)
