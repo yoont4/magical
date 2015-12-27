7191941fcf8983625f06fc69ea697a0dff31880b
ShangYiYang <shangyang824@gmail.com>
2015-12-26 18:13:16 -0800

    Enemy behaviour and basic fire properties added

--- enemy/enemyBehaviour.cs
+++ enemy/enemyBehaviour.cs
@@ -12,6 +12,8 @@ public class enemyBehaviour : MonoBehaviour {
     public float jumpHeight = 2;
     public bool onGround;
 
+    public LayerMask ground;
+
 	// Use this for initialization
 	void Start () {
         
@@ -19,19 +21,16 @@ public class enemyBehaviour : MonoBehaviour {
 
     void FixedUpdate() {
         int rand = Random.Range(0, 100);
-        //passiveHop(rand);
-        aggroHop(rand);
-
-
-
-
         dist = target.position.x - transform.position.x;
 
-        if (dist < lookAtDist) {
+        if (dist > lookAtDist) {
+            passiveHop(rand);
+        }else if (dist < lookAtDist) {
             lookAt();
         }
         if (dist < attackRange) {
             chase();
+            aggroHop(rand);
         }
     }
 
@@ -50,16 +49,15 @@ public class enemyBehaviour : MonoBehaviour {
     void chase() {
 
         if (dist > .75) {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         } else if (dist < -.75) {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
      }
 
     //when not chasing target random hops
     void passiveHop(int num) {
         if (num == 69 && onGround) {
-            //GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         }
     }
@@ -67,17 +65,21 @@ public class enemyBehaviour : MonoBehaviour {
     //when chasing target more frequent hops, hops vary, two heights
     void aggroHop(int num) {
         if (num == 25 && onGround) {
-            //GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         }else if (num == 1 && onGround) {
-            //GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight * 2), ForceMode2D.Impulse);
         }
     }
 
     //detects if on ground
     void OnCollisionEnter2D(Collision2D col) {
-        onGround = true;
+        // check if the colliding layer matches any of the ground layers, uses same concept as
+        // player onGround checker
+        int colBit = 1 << col.gameObject.layer;
+        int overlap = ground.value & colBit;
+        if (overlap > 0) {
+            onGround = true;
+        }
     }
 
     // detects if in air
--- mouse/directionalOutput.cs
+++ mouse/directionalOutput.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class directionalOutput : MonoBehaviour {
 	public Rigidbody2D castingItem; // a reference to the prefab to be cast
+    public Rigidbody2D castingItemActive; // reference to prefab, interacts with world but does not apply effects
     public float control;           // how "magentized" the object is
     public float lifeTime;          // how long the prefab lasts after releasing the mouse
 
     private Rigidbody2D newItem;    // copy of the cast item
+    private Rigidbody2D newItemActive; // copy of active cast item
     private float originalGravity; 
 	private Vector2 lastPos;        // used to track mouse position
 	
@@ -28,8 +30,15 @@ public class directionalOutput : MonoBehaviour {
             
 		} else if (Input.GetMouseButtonUp (0)) {
             // reapply gravity
-            newItem.gravityScale = originalGravity;
-			Cast (newItem);
+            //newItem.gravityScale = originalGravity;
+
+            // destroys initial non active cast item, creates active version of the item
+            // sets all settings of the non active to active
+            Destroy(newItem.gameObject);
+            newItemActive = (Rigidbody2D)Instantiate(castingItemActive, lastPos, transform.rotation);
+            newItemActive.velocity = newItem.velocity;
+            newItemActive.gravityScale = originalGravity;
+			Cast (newItemActive);
 		}
 	}
 
