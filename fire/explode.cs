4d3388a1b6623a95c11e8102abefc3b28f78acd6
ShangYiYang <shangyang824@gmail.com>
2015-12-26 18:20:46 -0800

    fire updated

---
+++ fire/explode.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explode : MonoBehaviour {
+    public GameObject explosion;
+    public bool trigger;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (trigger) {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D col) {
+        trigger = true;
+    }
+}
