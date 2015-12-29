46c70d1053178e2928298b5cc83f7b1bc339dbf7
ShangYiYang <shangyang824@gmail.com>
2015-12-28 20:59:55 -0800

    added fire particle prefab

--- fire/explode.cs
+++ fire/explode.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class explode : MonoBehaviour {
     public GameObject explosion;
+    public GameObject fire;
+    public GameObject fireParticle;
+    public float particleCount;
+    public float velocity;
     public bool trigger;
 
 	// Use this for initialization
@@ -11,14 +15,32 @@ public class explode : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 	    if (trigger) {
+            spawnParticles();
             Instantiate(explosion, transform.position, Quaternion.identity);
+            Instantiate(fire, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
 	}
 
     void OnCollisionEnter2D(Collision2D col) {
         trigger = true;
+    }
+
+    void spawnParticles() {
+        Vector2 vel;
+        for (int i = 0; i <= particleCount; i++) {
+
+            GameObject p = (GameObject) Instantiate(fireParticle, transform.position, Quaternion.identity);
+
+            vel = Random.insideUnitCircle * velocity;
+            vel.y += 2; // add upward bias
+                        // set particle direction/speed
+            p.GetComponent<Rigidbody2D>().velocity = vel;
+
+            // set destroy time
+            Destroy(p.gameObject, 2);
+        }
     }
 }
