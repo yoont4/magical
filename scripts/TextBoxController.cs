b3f853c8bd7a7c0533a586d617614f72798d6ca0
Justin Yoon <justin.o.yoon@gmail.com>
2015-12-31 21:41:51 -0800

    Switched to UI. Got box to break up lines and adjust size. Not correctly positioned though.

--- scripts/TextBoxController.cs
+++ scripts/TextBoxController.cs
@@ -1,41 +1,62 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TextBoxController : MonoBehaviour
 {
+
+    public static int MAX_WIDTH = 160;
+
     // The text to be displayed in the box.
-    public string script;
+    public string dialog;
 
-    // true if textbox should be displayed. False otherwise.
-    private bool displayText = false;
+    public Font font;
 
-    // Position of the object in world-space
-    private Vector3 objectPosition;
+    public int fontSize;
 
-    private Camera gameCamera;
-
-    void Awake()
-    {
-        objectPosition = gameObject.transform.position;
-        gameCamera = (Camera) FindObjectOfType(typeof(Camera));
-    }
-
-    void OnGUI()
-    {
-        if (displayText)
-        {
-            Vector3 convertedObjectPosition = gameCamera.WorldToScreenPoint(objectPosition + new Vector3(0, 0, 10));
-            GUI.Box(new Rect(convertedObjectPosition.x - 45, (Screen.height - convertedObjectPosition.y) - 125, 100, 100), script);
-        }
-    }
+    private Canvas canvas;
 
     public void open()
     {
-        displayText = true;
+        // Will add in newlines to break up long text. This is the broken up version.
+        string displayedDialog = dialog;
+
+        // index of the last spot where a newline was added
+        int lastNewlineIndex = 0;
+
+        for (int i = 25; i < displayedDialog.Length; i += 25)
+        {
+            
+            // Go through while loop until you find first whitespace.
+            int whiteSpaceLocator = i;
+            while (whiteSpaceLocator != lastNewlineIndex && !char.IsWhiteSpace(displayedDialog[whiteSpaceLocator - 1]))
+            {
+                whiteSpaceLocator--;
+            }
+
+            if (whiteSpaceLocator > lastNewlineIndex)
+            {
+                print("whitespace: " + whiteSpaceLocator);
+                print("lastNewine: " + lastNewlineIndex);
+                displayedDialog = displayedDialog.Insert(whiteSpaceLocator, System.Environment.NewLine);
+                lastNewlineIndex = whiteSpaceLocator + System.Environment.NewLine.Length;
+                i++;
+            }
+        }
+
+        GameObject canvasGameObject = (GameObject) Instantiate(Resources.Load("DialogCanvas"), transform.position, Quaternion.identity);
+        canvas = canvasGameObject.GetComponent<Canvas>();
+        Text text = canvas.GetComponentInChildren<Text>();
+        text.text = displayedDialog;
     }
 
     public void close()
     {
-        displayText = false;
+        if (canvas != null)
+        {
+            Destroy(canvas.GetComponent<CanvasScaler>());
+            Destroy(canvas.GetComponent<GraphicRaycaster>());
+            Destroy(canvas);
+        }
     }
 }
